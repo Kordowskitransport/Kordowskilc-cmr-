e172d280facaa5b7bfb5a6d3b20c4a1928940bb6
Kordowskitransport <kajgrz1e@gmail.com>
2026-06-26 16:13:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -232,3 +232,8 @@ app.get("/api/drivers",(req,res)=>{
     );
 
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Serwer działa na porcie ${PORT}`);
+});
